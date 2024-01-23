03b5a256d5347b45a4b6ad18db7ba0a7c97bfd7e
Seenivasababu <120184836+Seenivasababu@users.noreply.github.com>
2024-01-23 22:28:51 +0530

    Initial setup

---
+++ src/pages/Register.jsx
@@ -0,0 +1,8 @@
+
+const Register = () => {
+   return (
+     <h1 className='text-4xl'>Register</h1>
+   )
+ }
+ 
+ export default Register
---
+++ src/pages/SingleProduct.jsx
@@ -0,0 +1,8 @@
+
+const SingleProduct = () => {
+   return (
+     <h1 className='text-4xl'>SingleProduct</h1>
+   )
+ }
+ 
+ export default SingleProduct
